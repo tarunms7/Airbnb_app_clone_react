aadb585fad538aea14b1055b738fda53b6694e5d
tarun <apple@Apples-MacBook-Air.local>
2022-06-23 22:55:34 +0530

    Renamed MainContent.js to Navbar.js and also changed the imports accordingly

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import MainContent from './Components/MainContent';
+import Navbar from './Components/Navbar';
 import ImageGrid from './Components/ImageGrid';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div className='ALL_CONTENTS'>
-      <MainContent />
+      <Navbar />
       <ImageGrid />
   </div>
 );
