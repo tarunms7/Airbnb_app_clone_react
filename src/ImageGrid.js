2893f10d5006926ac439dbca9670178c19a5dd45
Tarun M <tam@synamedia.com>
2022-06-24 09:46:21 +0530

    Changed image grid logic

--- src/ImageGrid.js
+++ src/ImageGrid.js
@@ -8,11 +8,11 @@ import img5 from "./images/5.jpg"
 import img6 from "./images/6.jpg"
 import img7 from "./images/7.jpg"
 import img8 from "./images/11.jpg"
-
+import image_grid from "./images/image_grid.png"
 export default function ImageGrid(){
     return(
         <div className = "MainMainGrid">
-            <div className="MainGrid">
+            {/* <div className="MainGrid">
                 <div className = "ImageGrid1">
                     <img src = {img1}/>
                 </div>
@@ -32,7 +32,8 @@ export default function ImageGrid(){
                     <img src = {img8}/>
                 </div>
             
-            </div>
+            </div> */}
+            <img className = "image_grid"src = {image_grid}/>
             <div className = "HeroText">
                 <h2>Aquí hay una cita que cambiará su lige</h2>
                 <p>“Errar Es Humano, pero Más Lo Es Culpar de Ello a Otros” – “To Err Is Human, but It Is Even More So to Blame Others for It”</p>
